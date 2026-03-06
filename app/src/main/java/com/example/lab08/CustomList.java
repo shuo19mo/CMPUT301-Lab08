42e2be743a4cd28edcdfe657b4964c06c6af201b
Shuo <shuo19@ualberta.ca>
2026-03-06 09:42:02 -0700

    1123

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -16,7 +16,7 @@ public class CustomList {
     public boolean hasCity(City city) {
         return cities.contains(city);
     }
-
+//
     public int countCities() {
         return cities.size();
     }
