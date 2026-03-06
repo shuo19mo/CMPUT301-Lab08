4c15cd16f8d2e051e87a71cd7b9ebab62e388b92
Shuo <shuo19@ualberta.ca>
2026-03-06 09:29:53 -0700

    1123

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -3,5 +3,16 @@ package com.example.lab08;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
+    @Test
+    public void testHasCity() {
+
+        CustomList list = new CustomList();
+
+        City edmonton = new City("Edmonton", "Alberta");
+        list.addCity(edmonton);
+
+        assertTrue(list.hasCity(edmonton));
+
+    }
 
 }
